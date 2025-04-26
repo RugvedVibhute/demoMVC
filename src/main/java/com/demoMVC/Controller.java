1c08491c3e0dad357a29f0ed035b6ed202632fb7
RugvedVibhute <vibhuterugved@gmail.com>
2025-04-27 00:59:14 +0530

    updated code

--- src/main/java/com/demoMVC/Controller.java
+++ src/main/java/com/demoMVC/Controller.java
@@ -1,5 +1,18 @@
 package com.demoMVC;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    @RequestMapping("/add")
+    public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {
+        int k = i + j;
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("result");
+        mv.addObject("result", k);
+        return mv;
+    }
 }
